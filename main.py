e9956acfc2579865aa313af4860cd39fd2f8af9f
robertoop <olmos.201020856@gmail.com>
2023-05-23 09:48:54 -0600

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,6 @@ app=FastAPI()
 def index():
 	return "Hola a todos, quieres saber de pokemon"
 
-@app.get("/Pokemon")
-def pokemon():
-	return "Este es otro mensaje"
+@app.get("/Pokemon/{num}")
+def pokemon(num):
+	return num
