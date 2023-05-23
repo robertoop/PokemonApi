111df3b6ce90a75475f54fcb363ec5bc003d44cb
robertoop <olmos.201020856@gmail.com>
2023-05-23 09:55:45 -0600

    Update main.py

--- main.py
+++ main.py
@@ -9,3 +9,8 @@ def index():
 @app.get("/Pokemon/{num}")
 def pokemon(num):
 	return num
+
+@app.get("/Conversor_CaF/{C}")
+def conversorCaF(C):
+	TF=C*(9/5) + 32
+	return f"La temperatura es de {TF} grados Farenheit"
