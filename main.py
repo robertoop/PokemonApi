8c551f8dc8accf16af559b59eb5ff046e0113860
robertoop <olmos.201020856@gmail.com>
2023-05-23 11:10:10 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app=FastAPI()
 
@@ -15,7 +16,7 @@ def pokemon(num):
 	"4": "Charmander"
 		
 	}
-	return pokemons{num}
+	return pokemons[num]
 
 @app.get("/Conversor_CaF/{C}")
 def conversorCaF(C):
@@ -25,3 +26,22 @@ def conversorCaF(C):
 		return f"La temperatura es de {TF} grados Farenheit"
 	except:
 		return "Entrada invalida"
+@app.get("/RevisarEdad/{E1}/{E2}")	
+def revisar_edades(E1,E2):
+	E1=int(E1)
+	E2=int(E2)
+	if E1>E2+30:
+		return "Podrias ser su Abuelo"
+	elif E1>E2+15:
+		return "Podrias ser su padre"
+	elif E1>E2:
+		return "Eres mayor"
+	elif E2>E1+30:
+		return "Podria ser tu Abuelo"
+	elif E2>E1+15:
+		return "Podria ser tu padre"
+	elif E2>E1:
+		return "Es mayor que tu"
+	else:
+		return "Tienen la misma edad"
+	
