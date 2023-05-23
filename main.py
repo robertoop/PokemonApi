caf6fa6783ba4ffa0d8ec1bac178f0292ee06e7f
robertoop <olmos.201020856@gmail.com>
2023-05-23 10:04:52 -0600

    Update main.py

--- main.py
+++ main.py
@@ -12,5 +12,9 @@ def pokemon(num):
 
 @app.get("/Conversor_CaF/{C}")
 def conversorCaF(C):
-	TF=C*(9/5) + 32
-	return f"La temperatura es de {TF} grados Farenheit"
+	try:
+		C=float(C)
+		TF=C*(9/5) + 32
+		return f"La temperatura es de {TF} grados Farenheit"
+	except:
+		return "Entrada invalida"
